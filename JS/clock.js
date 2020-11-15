9ba2967108719665b0c35656e7c75a964ccafd61
Mani Bansal <44293399+ManiBansal0025@users.noreply.github.com>
2020-11-15 21:43:37 +0530

    Update clock.js

--- JS/clock.js
+++ JS/clock.js
@@ -1,71 +1,76 @@
+//Selectors
 const time = document.getElementById('time');
 const greeting = document.getElementById('greeting');
 const name = document.getElementById('name');
 const date = document.getElementById('date');
+//Event Handler
+name.addEventListener("keypress", setName);
+name.addEventListener("blur", setName);
 
-name.addEventListener("keypress",setName);
-name.addEventListener("blur",setName);
 
+//Functions
 function showtime() {
     let today = new Date();
     let hour = today.getHours();
     let min = today.getMinutes();
     let sec = today.getSeconds();
     let todaydate = today.toDateString();
-
-    const ampm = hour > 12 ? 'PM' : 'AM';
+    const amPm = hour > 12 ? 'PM' : 'AM';
     hour = hour % 12 || 12;
-
-    time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${ampm}`;
-    date.innerHTML=`${todaydate}`;
+    time.innerHTML = `${addZero(hour)}<span>:</span>${addZero(min)}<span>:</span>${addZero(sec)} ${amPm}`;
+    date.innerHTML = `${todaydate}`;
     setTimeout(showtime, 1000);
 }
 
 function addZero(n) {
-    return ((parseInt(n, 10) < 10 ? '0' : '')+n)
+    return (parseInt(n, 10) < 10 ? "0" : "") + n;
 }
-function setGreeting(){
-    let today=new Date();
-    let hour=today.getHours();
-    if(hour<12){
-        document.body.style.backgroundImage='url("../img/morning1.png")';
-        greeting.innerHTML='Good Morning';
-    }
-    else if(hour<18){
-        document.body.style.backgroundImage='url("../img/evening (1).jpg")';
-        greeting.innerHTML='Good Afternoon';
-    }else{
-        document.body.style.backgroundImage='url("../img/night (1).jpg")';
-        greeting.innerHTML='Good Evening';
-        document.body.style.color='white';
-    }
-}
-function getName(){
-    if(localStorage.getItem('mykeyname')===null)
-    {
-        name.innerHTML='[Enter Name]';
-    }
-    else
-    {
-        name.innerHTML=localStorage.getItem("mykeyname");
+
+function setGreeting() {
+    let today = new Date();
+    let hour = today.getHours();
+    if (hour < 12) {
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundImage = 'url("../images/morning.jpg")';
+        greeting.innerHTML = "Good Morning";
+        document.body.style.color = "white";
+
+    } else if (hour < 18) {
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundImage = 'url("../images/afternoon.jpg")';
+        greeting.innerHTML = "Good Afternoon";
+
+    } else {
+        document.body.style.backgroundSize = "cover";
+        document.body.style.backgroundImage = 'url("../images/evening.jpg")';
+
+        greeting.innerHTML = "Good Evening";
+        document.body.style.color = "white";
     }
 
+
 }
-function setName(e){
-    if(e.type==="keypress")
-    {
-        if(e.keyCode==13){
-            localStorage.setItem("mykeyname",e.target.innerHTML);    
+
+function getName() {
+    if (localStorage.getItem("mykeyname") == null) {
+        name.innerHTML = '[Enter name]';
+    } else {
+        name.innerHTML = localStorage.getItem("mykeyname");
+    }
+}
+
+function setName(e) {
+    if (e.type === "keypress") {
+        if (e.keyCode == 13) {
+            localStorage.setItem("mykeyname", e.target.innerHTML);
             name.blur();
         }
-    }
-    else
-    {
-        localStorage.setItem("mykeyname",e.target.innerHTML);
+    } else {
+        localStorage.setItem("mykeyname", e.target.innerHTML);
     }
 }
 
-
+//Functions call
 showtime();
 setGreeting();
-getName()
+getName();
